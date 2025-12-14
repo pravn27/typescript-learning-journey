6a27c746a34e78b479dd06179c9472cf442dae9a
pravn27 <praveen.sri27@gmail.com>
2025-12-14 08:09:16 +0530

    Typescript annotation, inference basics

---
+++ courses/from-colt-steele/2-types/variables.js
@@ -0,0 +1,15 @@
+// type string
+var movieTitle = "3 Idiots";
+// after 1 month
+movieTitle = "Super";
+movieTitle.tooUpperCase();
+// after 2 months
+movieTitle = 12;
+// type number & boolean
+var userAge = 34;
+userAge = userAge + 2;
+userAge = 40;
+userAge = "zero";
+var isTypeScriptEasy = true;
+isTypeScriptEasy = false;
+isTypeScriptEasy = "true";
---
+++ courses/from-colt-steele/2-types/variables.ts
@@ -0,0 +1,24 @@
+// type string
+let movieTitle: string = "3 Idiots";
+// after 1 month
+movieTitle = "Super";
+movieTitle.tooUpperCase();
+// after 2 months
+movieTitle = 12;
+
+// type number & boolean
+let userAge: number = 34;
+userAge = userAge + 2;
+userAge = 40;
+userAge = "zero";
+
+let isTypeScriptEasy: boolean = true;
+isTypeScriptEasy = false;
+isTypeScriptEasy = "true";
+
+// Type Inference
+let fullName = "Praveen S";
+fullName = 12;
+
+let isCool = false;
+isCool = "true";
