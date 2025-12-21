c009bee325a4863bfcb32cc7389fafe6beb25b33
pravn27 <praveen.sri27@gmail.com>
2025-12-21 18:41:58 +0530

    Typescript object type, its practice

--- courses/from-colt-steele/4-objects/objects_types.ts
+++ courses/from-colt-steele/4-objects/objects_types.ts
@@ -47,3 +47,75 @@ const user1 = {
 };
 
 getUserDetails(user1);
+
+// Nested object types annotation
+type Company = {
+  name: string;
+  branch: string[];
+  headQuarters: {
+    street: string;
+    city: string;
+    zipCode: number;
+  };
+};
+
+const techCompany: Company = {
+  name: "Xactly corp",
+  branch: ["IN", "CA", "DE"],
+  headQuarters: {
+    street: "Innovation tech park",
+    city: "Bangalore",
+    zipCode: 1211121,
+  },
+};
+
+function getCompanyLocation(company: Company): void {
+  console.log(`Location is ${company.headQuarters.city}`);
+}
+
+getCompanyLocation(techCompany);
+
+// optional properties using ?, z point is optional
+type Point1 = {
+  x: number;
+  y: number;
+  z?: number;
+};
+
+const dPoint: Point1 = {
+  x: 23,
+  y: 43,
+};
+
+// readonly modifier to property, make is prop as read only
+// readonly is from typescript layer
+type User = {
+  readonly id: number;
+  name: string;
+};
+const user: User = {
+  id: 1311,
+  name: "Praveen",
+};
+console.log(user.id);
+// user.id = 4455;
+
+// Intersection types using &, for combining 2 or more types
+type Circle = {
+  radius: number;
+};
+
+type Colorful = {
+  color: string;
+};
+
+type ColorFulCircle = Circle &
+  Colorful & {
+    diameter: number;
+  };
+
+const happyFace: ColorFulCircle = {
+  radius: 24,
+  color: "red",
+  diameter: 12,
+};
