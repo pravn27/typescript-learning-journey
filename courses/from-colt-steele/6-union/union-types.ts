335a49bf6c757b1ef0bb70a5b1fd720176348a81
pravn27 <praveen.sri27@gmail.com>
2025-12-22 22:40:15 +0530

    Typescript union types

--- courses/from-colt-steele/6-union/union-types.ts
+++ courses/from-colt-steele/6-union/union-types.ts
@@ -1,3 +1,20 @@
 // union types
 let age: number | string = 23;
 age = "24";
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+type ThreeDPoint = {
+  x: number;
+  y: number;
+  z: number;
+};
+
+const graph: Point | ThreeDPoint = {
+  x: 23,
+  y: 34,
+  z: 22,
+};
