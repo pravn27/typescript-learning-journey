f6d0af0db379c65661164e3da0a6be71e5ae6d74
pravn27 <praveen.sri27@gmail.com>
2025-12-23 22:15:30 +0530

    Typescript tuples

---
+++ courses/from-colt-steele/7-tuples-enums/tuples-enums.ts
@@ -0,0 +1,8 @@
+// tuples - arrays with fixed length & types
+const rgbColor: [number, number, number] = [255, 125, 254];
+
+type HttpResponse = [number, string];
+const goodRes: HttpResponse = [200, "ok, Getting proper response"];
+// goodRes[0] = "test";
+
+// enums
