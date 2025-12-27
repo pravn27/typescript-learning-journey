7b66145e4d758e94831225fdff3664750a772b5c
pravn27 <praveen.sri27@gmail.com>
2025-12-27 19:22:36 +0530

    Typescript enums

--- courses/from-colt-steele/7-tuples-enums/tuples-enums.ts
+++ courses/from-colt-steele/7-tuples-enums/tuples-enums.ts
@@ -5,4 +5,12 @@ type HttpResponse = [number, string];
 const goodRes: HttpResponse = [200, "ok, Getting proper response"];
 // goodRes[0] = "test";
 
-// enums
+// enums - define set of named constant, reuse in code
+enum OrderStatus {
+  PENDING,
+  SHIPPED,
+  DELIVERED,
+  RETURNED,
+}
+
+const myStatus = OrderStatus.DELIVERED;
